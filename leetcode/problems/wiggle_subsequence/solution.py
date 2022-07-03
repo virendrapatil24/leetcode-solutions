94efde2c3e48e8d6bab1adff199d75774f4a2245
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2022-07-03 05:44:59 +0000

    Sync LeetCode submission - Wiggle Subsequence (python3)

---
+++ leetcode/problems/wiggle_subsequence/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def wiggleMaxLength(self, nums):
+        nan = float('nan')
+        diffs = [a-b for a, b in zip([nan] + nums, nums + [nan]) if a-b]
+        return sum(not d*e >= 0 for d, e in zip(diffs, diffs[1:]))
+        
