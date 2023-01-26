34ce096ce4d92278be11b7daff771759592d1e0b
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2023-01-26 13:08:35 +0000

    Sync LeetCode submission - Majority Element (python3)

--- leetcode/problems/majority_element/solution.py
+++ leetcode/problems/majority_element/solution.py
@@ -1,6 +1,3 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        #ans = Counter(nums).most_common(1)
-        #return ans[0][0]
-        
         return Counter(nums).most_common(1)[0][0]
