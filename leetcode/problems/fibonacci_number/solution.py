3368421601ae80f6829b9b2a08abaa820a9d49c8
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-01-06 10:41:46 +0000

    Sync LeetCode submission - Fibonacci Number (python3)

--- leetcode/problems/fibonacci_number/solution.py
+++ leetcode/problems/fibonacci_number/solution.py
@@ -1,10 +1,6 @@
 class Solution:
-    def fib(self, n: int, cache={}) -> int:
+    def fib(self, n: int) -> int:
         if n < 2:
             return n
+        return self.fib(n-1) + self.fib(n-2)
         
-        if n in cache:
-            return cache[n]
-
-        cache[n] = self.fib(n-1, cache) + self.fib(n-2, cache)
-        return cache[n]
