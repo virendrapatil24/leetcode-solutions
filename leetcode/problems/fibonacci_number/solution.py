42e528de90a196f6ff06f77cd61582f98711615d
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-01-07 04:24:39 +0000

    Sync LeetCode submission - Fibonacci Number (python3)

--- leetcode/problems/fibonacci_number/solution.py
+++ leetcode/problems/fibonacci_number/solution.py
@@ -2,5 +2,8 @@ class Solution:
     def fib(self, n: int) -> int:
         if n < 2:
             return n
-        return self.fib(n-1) + self.fib(n-2)
+        a, b = 0, 1
+        for i in range(n):
+            a, b = b, a+b
+        return a
         
