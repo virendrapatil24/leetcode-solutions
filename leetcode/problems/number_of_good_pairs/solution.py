7f0ef2f04ea4d93e88256e855afbb57399eaf77f
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-01-08 17:40:29 +0000

    Sync LeetCode submission - Number of Good Pairs (python3)

--- leetcode/problems/number_of_good_pairs/solution.py
+++ leetcode/problems/number_of_good_pairs/solution.py
@@ -1,8 +1,9 @@
 class Solution:
-    def numIdenticalPairs(self, num: List[int]) -> int:
-        count = 0
-        for i in range(len(num)):
-            for j in range(i+1,len(num)):
-                if num[i] == num[j]: count += 1
-                    
-        return count
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        res = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    res += 1
+        return res
+        
