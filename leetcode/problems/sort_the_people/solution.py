b20777826b4157376a737520d0bff84a92e6fff0
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-01-12 15:05:02 +0000

    Sync LeetCode submission - Sort the People (python3)

--- leetcode/problems/sort_the_people/solution.py
+++ leetcode/problems/sort_the_people/solution.py
@@ -1,3 +1,4 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        return [row[0] for row in sorted(map(list, zip(names, heights)), key=lambda a:a[1], reverse=True)]
+        return [a[0] for a in sorted(list(zip(names, heights)), key=lambda a:a[1], reverse=True)]
+        
