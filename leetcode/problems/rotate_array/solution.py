f913c2a64455ca2c02143eb73478aa000d9687b9
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-01-14 16:15:26 +0000

    Sync LeetCode submission - Rotate Array (python3)

---
+++ leetcode/problems/rotate_array/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+        nums[n-k:] = reversed(nums[n-k:])
+        nums[:n-k] = reversed(nums[:n-k])
+        nums.reverse()
+        
+        
