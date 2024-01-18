157f188cbd0f27b9ce39605907d9d4181e47d0a9
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-01-18 03:44:59 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- leetcode/problems/climbing_stairs/solution.py
+++ leetcode/problems/climbing_stairs/solution.py
@@ -1,14 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        cache = {}
-        def climb(n, cache):
-            if n <= 2:
-                return n
-            
-            if n in cache:
-                return cache[n]
+        if n <= 2:
+            return n
+        i, j =  1, 1
+        for k in range(1, n):
+            i, j = j, i+j
+        return j
 
-            cache[n] = climb(n-2, cache) + climb(n-1, cache)
-            return cache[n]
-        return climb(n, cache)
+    
+
         
