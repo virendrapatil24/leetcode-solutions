f6415e42ccac92f4de7e94a77cd659bf990d7106
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-01-26 10:23:47 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

--- leetcode/problems/valid_parentheses/solution.py
+++ leetcode/problems/valid_parentheses/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         if len(s) % 2 != 0: return False
-        brackets = {'(':')', '{':'}', '[':']'}
+        isMatch = {'(':')', '{':'}', '[':']'}
         stack = []
         for char in s:
-            if char in brackets:
+            if char in isMatch:
                 stack.append(char)
-            elif len(stack) == 0 or brackets[stack.pop()] != char:
+            elif len(stack) == 0 or isMatch[stack.pop()] != char: 
                 return False
         return len(stack) == 0
+                
         
