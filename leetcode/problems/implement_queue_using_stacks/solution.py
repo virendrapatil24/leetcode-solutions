c19d9e45b091176391aa9e6963e37a94a1bf392a
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-01-29 06:25:49 +0000

    Sync LeetCode submission - Implement Queue using Stacks (python3)

--- leetcode/problems/implement_queue_using_stacks/solution.py
+++ leetcode/problems/implement_queue_using_stacks/solution.py
@@ -4,22 +4,25 @@ class MyQueue:
         self.in_stack = []
         self.out_stack = []
         
+
     def push(self, x: int) -> None:
         self.in_stack.append(x)
         
+
     def pop(self) -> int:
         self.peek()
         return self.out_stack.pop()
         
+
     def peek(self) -> int:
         if not self.out_stack:
             while self.in_stack:
                 self.out_stack.append(self.in_stack.pop())
         return self.out_stack[-1]
         
+
     def empty(self) -> bool:
         return not self.in_stack and not self.out_stack
-
         
 
 
