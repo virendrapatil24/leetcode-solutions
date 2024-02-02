136d4c1b4dcf7aaf018cf26e4b6535c0e4006251
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-02-02 13:17:19 +0000

    Sync LeetCode submission - Delete Node in a BST (python3)

---
+++ leetcode/problems/delete_node_in_a_bst/solution.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                succ = self.successor(root.right)
+                root.val = succ
+                root.right = self.deleteNode(root.right, succ)
+        return root
+
+    def successor(self, curr):
+        while curr.left:
+            curr = curr.left
+        return curr.val
+
+        
