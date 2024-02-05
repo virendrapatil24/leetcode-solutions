f1c50c0dfcf1a635e8ace323be92ef7b05586d6d
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-02-05 04:05:04 +0000

    Sync LeetCode submission - First Unique Character in a String (python3)

--- leetcode/problems/first_unique_character_in_a_string/solution.py
+++ leetcode/problems/first_unique_character_in_a_string/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        
-        for ind, i in enumerate(s):
-            
-            if s.count(i) == 1: return ind
-        
+        map_char = {}
+        for char in s:
+            map_char[char] = map_char.get(char, 0) + 1
+        for idx, char in enumerate(s):
+            if map_char[char] == 1:
+                return idx
         return -1
-                
+        
