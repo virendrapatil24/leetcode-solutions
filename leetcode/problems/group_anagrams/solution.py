2d45f964dc80a686ee728c3691ba9859a20049c0
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-02-06 11:28:35 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- leetcode/problems/group_anagrams/solution.py
+++ leetcode/problems/group_anagrams/solution.py
@@ -2,10 +2,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         mapAgm = {}
         for s in strs:
-            if tuple(sorted(s)) in mapAgm.keys():
-                mapAgm[tuple(sorted(s))].append(s)
-            else:
-                mapAgm[tuple(sorted(s))] = [s]
+            mapAgm[tuple(sorted(s))] = mapAgm.get(tuple(sorted(s)), []) +[s]
         return mapAgm.values()
 
 
