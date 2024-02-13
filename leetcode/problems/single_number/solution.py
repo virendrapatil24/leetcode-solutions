48a3521309eaad2bceb430f1e374f1a39ddcf097
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-02-13 13:43:52 +0000

    Sync LeetCode submission - Single Number (python3)

--- leetcode/problems/single_number/solution.py
+++ leetcode/problems/single_number/solution.py
@@ -1,10 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
-        set_nums = set(nums)
-        
-        for i in set_nums:
-            if nums.count(i) == 1: return i
-            
-     
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
         
