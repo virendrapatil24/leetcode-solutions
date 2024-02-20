b8450bd50166e96dbbb9637b3f0eeb3f99ff645a
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-02-20 15:57:32 +0000

    Sync LeetCode submission - Missing Number (python3)

--- leetcode/problems/missing_number/solution.py
+++ leetcode/problems/missing_number/solution.py
@@ -1,9 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        #if
-        for i in range(0,nums[-1]):
-            if i != nums[i]:
-                return i
-        return len(nums)
+        n = len(nums)
+        return (n * (n + 1) // 2) - sum(nums)
+
         
