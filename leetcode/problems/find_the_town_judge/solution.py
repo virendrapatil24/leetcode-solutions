bed9ddb2231bb55cb7a7d1c3b396e5a207052a80
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-02-22 05:09:07 +0000

    Sync LeetCode submission - Find the Town Judge (python3)

--- leetcode/problems/find_the_town_judge/solution.py
+++ leetcode/problems/find_the_town_judge/solution.py
@@ -1,12 +1,11 @@
 class Solution:
-    def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        count = [0] * (n + 1)
-        
-        for i, j in trust:
-            count[i] -= 1
-            count[j] += 1
-                
-        for i in range(1,n+1):
-            if count[i] == n - 1:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        in_degree = [0] * (N + 1)
+        out_degree = [0] * (N + 1)
+        for a in trust:
+            out_degree[a[0]] += 1
+            in_degree[a[1]] += 1
+        for i in range(1, N + 1):
+            if in_degree[i] == N - 1 and out_degree[i] == 0:
                 return i
         return -1
