5d8a8a88208f53f7b68e23303b66898b24b81948
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-03-25 05:31:50 +0000

    Sync LeetCode submission Runtime - 117 ms (46.15%), Memory - 19.9 MB (92.35%)

---
+++ leetcode/0384-shuffle-an-array/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.original = list(nums)
+        
+
+    def reset(self) -> List[int]:
+        return self.original
+        
+
+    def shuffle(self) -> List[int]:
+        for i in range(len(self.nums)):
+            j = randint(i, len(self.nums)-1)
+            self.nums[i], self.nums[j] = self.nums[j], self.nums[i]
+        return self.nums
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
