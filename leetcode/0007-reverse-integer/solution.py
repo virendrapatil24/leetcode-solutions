37b6ff3225b19e33c2dac4e89e02a96b5f0c2150
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-04-21 15:16:39 +0000

    Sync LeetCode submission Runtime - 35 ms (67.31%), Memory - 16.6 MB (68.94%)

--- leetcode/0007-reverse-integer/solution.py
+++ leetcode/0007-reverse-integer/solution.py
@@ -1,7 +1,15 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x < 0: res = -int(str(x)[-1:0:-1])
-        else: res = int(str(x)[::-1])
-        return res if -2 ** 31 < res  and res < 2 ** 31 - 1 else 0
+        def reverse_num(pos_num):
+            rev = 0
+            while pos_num > 0:
+                rev = rev * 10 + pos_num % 10
+                pos_num //= 10
+            return rev
+        if x < 0:
+            res = reverse_num(x * -1) * -1
+        else:
+            res = reverse_num(x)
+        return res if -2 ** 31 < res and res < 2 ** 31 - 1 else 0
 
         
