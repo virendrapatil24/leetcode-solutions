3a930c8efce295abefe1a0d6334da358d6ed3801
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-04-27 09:40:41 +0000

    Sync LeetCode submission Runtime - 343 ms (54.57%), Memory - 33.3 MB (90.13%)

---
+++ leetcode/1604-least-number-of-unique-integers-after-k-removals/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        map_count = {}
+        for num in arr:
+            map_count[num] = map_count.get(num, 0) + 1
+        heap = list(map_count.values())
+        heapify(heap)
+        while k > 0:
+            val = heappop(heap)
+            k -= val
+        return len(heap) if k == 0 else len(heap) + 1
