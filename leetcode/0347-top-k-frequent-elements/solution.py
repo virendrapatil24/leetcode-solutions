95d0f7e74795ff9ba7d5314739efcba8792de593
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-05-02 17:50:06 +0000

    Sync LeetCode submission Runtime - 95 ms (27.31%), Memory - 21.2 MB (56.66%)

--- leetcode/0347-top-k-frequent-elements/solution.py
+++ leetcode/0347-top-k-frequent-elements/solution.py
@@ -5,9 +5,12 @@ class Solution:
             map_count[num] = map_count.get(num, 0) + 1
         heap = []
         for key, val in map_count.items():
-            if len(heap) == k:
+            if len(heap) >= k:
                 heappushpop(heap, (val, key))
             else:
                 heappush(heap, (val, key))
-        return [ k for v, k in heap]
+        res = []
+        for i in range(k):
+            res.append(heappop(heap)[1])
+        return res
         
