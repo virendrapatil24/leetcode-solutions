5f16e051af1b707827c211687aa328ed21f9482c
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-05-08 04:58:08 +0000

    Sync LeetCode submission Runtime - 70 ms (40.61%), Memory - 18 MB (22.65%)

--- leetcode/0506-relative-ranks/solution.py
+++ leetcode/0506-relative-ranks/solution.py
@@ -3,14 +3,12 @@ class Solution:
         heap = []
         for idx, scr in enumerate(score):
             heappush(heap, (-scr, idx))
-
+        
         medal_map = {1: "Gold Medal", 2: "Silver Medal", 3: "Bronze Medal"}
 
         res = [0] * len(score)
-
-        for rank in range(1, len(score)+1):
-            _, idx = heappop(heap)
+        for rank in range(1, len(score) + 1):
+            idx = heappop(heap)[1]
             res[idx] = medal_map.get(rank, str(rank))
-
-        return res
         
+        return res
