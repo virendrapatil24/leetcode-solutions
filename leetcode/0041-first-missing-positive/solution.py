eb7a79d90c9500018a9277dcadb1785d9d86e2fd
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-05-18 07:21:20 +0000

    Sync LeetCode submission Runtime - 403 ms (5.14%), Memory - 30.1 MB (98.09%)

--- leetcode/0041-first-missing-positive/solution.py
+++ leetcode/0041-first-missing-positive/solution.py
@@ -1,12 +1,14 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        nums_set = set()
-        for num in nums:
-            if num > 0:
-                nums_set.add(num)
-        for i in range(1, len(nums)+2):
-            if i not in nums_set:
-                return i
-        return -1
+        for i in range(len(nums)):
+            while 1 <= nums[i] <= len(nums) and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return i + 1
+        
+        return len(nums) + 1
+
             
         
