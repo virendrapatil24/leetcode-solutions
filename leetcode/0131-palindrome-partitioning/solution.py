1a98401916cbcce8fb3ce5956b6b187c1be0ed4d
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-05-22 12:01:37 +0000

    Sync LeetCode submission Runtime - 470 ms (43.97%), Memory - 35.3 MB (17.38%)

---
+++ leetcode/0131-palindrome-partitioning/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        lenS = len(s)
+        def explore(index, curr):
+            if index >= lenS:
+                result.append(curr[:])
+            
+            for i in range(index, lenS):
+                subStr = s[index : i + 1]
+                if subStr == subStr[::-1]:
+                    curr.append(subStr)
+                    explore(i + 1, curr)
+                    curr.pop()
+            
+        explore(0, [])
+        return result
+        
