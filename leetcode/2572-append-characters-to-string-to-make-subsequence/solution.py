9e4a6182be753393e869f6d25f10023b6608356f
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-06-03 08:05:12 +0000

    Sync LeetCode submission Runtime - 61 ms (62.62%), Memory - 17.6 MB (72.52%)

---
+++ leetcode/2572-append-characters-to-string-to-make-subsequence/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        i, j = 0, 0
+        m, n = len(s), len(t)
+        while i < m and j < n:
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                i += 1
+        return n - j
+        
