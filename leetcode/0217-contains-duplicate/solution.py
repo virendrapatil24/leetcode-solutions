af173e2e9f83b82fb760ab265473a443a6880d87
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-06-17 15:57:27 +0000

    Sync LeetCode submission Runtime - 476 ms (5.22%), Memory - 31.9 MB (80.10%)

--- leetcode/0217-contains-duplicate/solution.py
+++ leetcode/0217-contains-duplicate/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums_set = set()
+        hashMap = set()
         for num in nums:
-            if num in nums_set: return True
-            else: nums_set.add(num)
+            if num in hashMap:
+                return True
+            hashMap.add(num)
         return False
         
