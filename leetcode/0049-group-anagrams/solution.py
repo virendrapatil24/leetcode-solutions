c3d5b2777ef2a0297f94c4b049bbc47c9fc1e57d
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-06-17 16:39:43 +0000

    Sync LeetCode submission Runtime - 100 ms (20.66%), Memory - 20.7 MB (39.10%)

--- leetcode/0049-group-anagrams/solution.py
+++ leetcode/0049-group-anagrams/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        map_angm = {}
-        for strg in strs:
-            map_angm[tuple(sorted(strg))] = map_angm.get(tuple(sorted(strg)), []) + [strg]
-        return map_angm.values()
+        hashMap = {}
+        for word in strs:
+            hashMap[tuple(sorted(word))] = hashMap.get(tuple(sorted(word)), []) + [word]
+        return hashMap.values()
 
