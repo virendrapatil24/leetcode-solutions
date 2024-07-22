94e22933e50f56a32ab7f85784f643f40e79cb5a
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2024-07-22 05:08:44 +0000

    Sync LeetCode submission Runtime - 98 ms (81.66%), Memory - 17.1 MB (27.57%)

---
+++ leetcode/2502-sort-the-people/solution.py
@@ -0,0 +1,4 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        return [x[0] for x in sorted(zip(names, heights), key = lambda x: x[1], reverse = True)]
+        
