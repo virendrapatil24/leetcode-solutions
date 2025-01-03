2097ae8b6fee40acdc9cd8e99b109d874aa9413d
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2025-01-03 15:48:30 +0000

    Sync LeetCode submission Runtime - 32 ms (84.58%), Memory - 17.8 MB (11.29%)

--- leetcode/1013-fibonacci-number/solution.py
+++ leetcode/1013-fibonacci-number/solution.py
@@ -1,9 +1,8 @@
 class Solution:
-    def fib(self, n: int, cache={}) -> int:
+    def fib(self, n: int) -> int:
         if n < 2:
             return n
-        if n in cache:
-            return cache[n]
-        cache[n] = self.fib(n-1) + self.fib(n-2) 
-        return cache[n]
-        
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
