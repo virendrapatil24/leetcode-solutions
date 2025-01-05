7f27efffe966a658dc95fcc451d79ae44c0940c5
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2025-01-05 17:51:19 +0000

    Sync LeetCode submission Runtime - 3 ms (53.82%), Memory - 19 MB (12.00%)

--- leetcode/0001-two-sum/solution.py
+++ leetcode/0001-two-sum/solution.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map_sum = {}
-        for idx, num in enumerate(nums):
-            if num not in map_sum:
-                map_sum[target - num] = idx
-            else:
-                return [idx, map_sum[num]]
+        hash_map = {}
+        for i, n in enumerate(nums):
+            if target - n in hash_map:
+                return [hash_map[target - n], i]
+            hash_map[n] = i
+        
