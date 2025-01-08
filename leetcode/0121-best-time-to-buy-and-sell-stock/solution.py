f04e354c4c4de176703a89aa04ea4c7ef3b4a9c2
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2025-01-08 15:44:00 +0000

    Sync LeetCode submission Runtime - 115 ms (38.07%), Memory - 26.8 MB (30.24%)

--- leetcode/0121-best-time-to-buy-and-sell-stock/solution.py
+++ leetcode/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,10 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        min_val, max_profit = prices[0], 0
+        min_val, max_profit = inf, 0
         for price in prices:
-            if price < min_val:
-                min_val = price
-            if max_profit < price - min_val:
-                max_profit = price - min_val
+            min_val = min(min_val, price)
+            max_profit = max(max_profit, price - min_val)
         return max_profit
         
