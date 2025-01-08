2a5db162bea2527f56f0e8e5222c22af46c90d3b
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2025-01-08 17:59:10 +0000

    Sync LeetCode submission Runtime - 6 ms (61.88%), Memory - 19.4 MB (19.88%)

--- leetcode/0169-majority-element/solution.py
+++ leetcode/0169-majority-element/solution.py
@@ -1,4 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return sorted(nums)[len(nums)//2]
+        el, count = nums[0], 1
+        for i in range(1, len(nums)):
+            if count != 0:
+                count += 1 if nums[i] == el else -1
+            else:
+                el = nums[i]
+                count = 1
+        return el
         
