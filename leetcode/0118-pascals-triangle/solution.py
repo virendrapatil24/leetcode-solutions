9bc53025312e6f61ed84403b7baadac3cee37756
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2025-01-12 09:25:27 +0000

    Sync LeetCode submission Runtime - 1 ms (16.03%), Memory - 17.7 MB (37.60%)

--- leetcode/0118-pascals-triangle/solution.py
+++ leetcode/0118-pascals-triangle/solution.py
@@ -1,18 +1,14 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        
-        ans = []
-        
+        res = []
         for i in range(numRows):
             temp = []
-            for j in range(i+1):
-                
-                if j == 0 or j == i:
+            for j in range(i + 1):
+                if j in [0, i]:
                     temp.append(1)
                 else:
-                    k = ans[i-1][j-1] + ans[i-1][j]
-                    temp.append(k)
-                    
-            ans.append(temp)
+                    temp.append(res[-1][j] + res[-1][j - 1])
+            res.append(temp)
+        return res
+
         
-        return ans
