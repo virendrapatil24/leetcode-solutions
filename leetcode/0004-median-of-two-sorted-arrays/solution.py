1af53df0a84a05c5fe2d15147cd9a8ef21e060cd
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2025-02-06 16:16:03 +0000

    Sync LeetCode submission Runtime - 1 ms (65.93%), Memory - 18.2 MB (20.45%)

--- leetcode/0004-median-of-two-sorted-arrays/solution.py
+++ leetcode/0004-median-of-two-sorted-arrays/solution.py
@@ -1,31 +1,32 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        m, n = len(nums1), len(nums2)
-        if m > n:
-            nums1, nums2 = nums2, nums1
-            m, n = n, m
+    def findMedianSortedArrays(self, nums1, nums2):
+        n = len(nums1)
+        m = len(nums2)
+        i = 0
+        j = 0
+        m1 = 0
+        m2 = 0
 
-        l, r = 0, m
-        while l <= r:
-            i1 = (l + r) // 2
-            i2 = ((m + n + 1) // 2) - i1
-
-            min_r1 = inf if i1 >= m else nums1[i1]
-            max_l1 = -inf if i1 <= 0 else nums1[i1 - 1]
-
-            min_r2 = inf if i2 >= n else nums2[i2]
-            max_l2 = -inf if i2 <= 0 else nums2[i2 - 1]
-
-            if max_l2 <= min_r1 and max_l1 <= min_r2:
-                if (m + n) % 2 == 0:
-                    return (max(max_l1, max_l2) + min(min_r1, min_r2)) / 2
+        # Find median.
+        for count in range(0, (n + m) // 2 + 1):
+            m2 = m1
+            if i < n and j < m:
+                if nums1[i] > nums2[j]:
+                    m1 = nums2[j]
+                    j += 1
                 else:
-                    return max(max_l1, max_l2)
-            elif max_l1 > min_r2:
-                r = i1 - 1
+                    m1 = nums1[i]
+                    i += 1
+            elif i < n:
+                m1 = nums1[i]
+                i += 1
             else:
-                l = i1 + 1
-            
+                m1 = nums2[j]
+                j += 1
 
-            
-        
+        # Check if the sum of n and m is odd.
+        if (n + m) % 2 == 1:
+            return float(m1)
+        else:
+            ans = float(m1) + float(m2)
+            return ans / 2.0
