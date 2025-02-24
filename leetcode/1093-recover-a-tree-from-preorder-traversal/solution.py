7560eca95fb7a6243c8e52b5b03505ec71c05c7e
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2025-02-24 16:57:22 +0000

    Sync LeetCode submission Runtime - 14 ms (84.24%), Memory - 18.5 MB (40.60%)

--- leetcode/1093-recover-a-tree-from-preorder-traversal/solution.py
+++ leetcode/1093-recover-a-tree-from-preorder-traversal/solution.py
@@ -4,42 +4,42 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-
 class Solution:
     def recoverFromPreorder(self, traversal: str) -> Optional[TreeNode]:
-        levels = []  # List to track the last node at each depth
-        index, n = 0, len(traversal)
-
-        while index < n:
-            # Count depth (number of dashes)
-            depth = 0
-            while index < n and traversal[index] == "-":
+        n = len(traversal)
+        levels = []
+        i = 0
+        while i < n:
+            
+            depth = 1
+            while i < n and traversal[i] == "-":
                 depth += 1
-                index += 1
+                i += 1
 
-            # Extract node value
-            value = 0
-            while index < n and traversal[index].isdigit():
-                value = value * 10 + int(traversal[index])
-                index += 1
+            val = ""
+            while i < n and traversal[i].isdigit():
+                val += traversal[i]
+                i += 1
+            val = int(val)
 
-            # Create the new node
-            node = TreeNode(value)
-
-            # Adjust levels list to match the current depth
-            if depth < len(levels):
-                levels[depth] = node
-            else:
-                levels.append(node)
-
-            # Attach the node to its parent
-            if depth > 0:
-                parent = levels[depth - 1]
-                if parent.left is None:
-                    parent.left = node
+            while levels and len(levels) > depth:
+                levels.pop()
+            
+            node = TreeNode(val)
+            if levels:
+                if levels[-1].left == None:
+                    levels[-1].left = node
                 else:
-                    parent.right = node
-
-        # The root node is always at index 0
+                    levels[-1].right = node
+            
+            levels.append(node)
+            i += 1
+        
         return levels[0]
+        
+
+
+            
+            
+                
+        
