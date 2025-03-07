fbc92eca9718db7931aece02b7f76ecca159c80c
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2025-03-07 17:18:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (64.86%)

--- leetcode/0078-subsets/solution.py
+++ leetcode/0078-subsets/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def create_subsets(start, path):
-            res.append(path)
-            for i in range(start, len(nums)):
-                create_subsets(i + 1, path + [nums[i]])
         res = []
-        create_subsets(0, [])
+        def create_subset(idx, temp):
+            res.append(temp)
+            for i in range(idx, len(nums)):
+                create_subset(i + 1, temp + [nums[i]])
+        create_subset(0, [])
         return res
 
-             
+        
