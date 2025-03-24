94a489336981fd7059090f16c5a536e56a5345b7
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2025-03-24 06:04:01 +0000

    Sync LeetCode submission Runtime - 20 ms (53.90%), Memory - 17.8 MB (57.12%)

--- leetcode/0040-combination-sum-ii/solution.py
+++ leetcode/0040-combination-sum-ii/solution.py
@@ -1,21 +1,26 @@
 class Solution:
-    def combinationSum2(self, cand, target):
-        cand.sort()
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
         res = []
-        path = []
-        self.dfs(cand, 0, target, path, res)
-        return res
-    
-    def dfs(self, cand, cur, target, path, res):
-        if target == 0:
-            res.append(path.copy())
-            return
-        if target < 0:
-            return
+        candidates.sort()
+
+        def dfs(curr_sum, stack, idx):
+            if curr_sum == target:
+                res.append(stack[:])
+                return
+            
+            if curr_sum > target or idx >= n:
+                return
+            
+            for i in range(idx, n):
+                if i > idx and candidates[i] == candidates[i - 1]:
+                    continue
+                stack.append(candidates[i])
+                curr_sum += candidates[i]
+                dfs(curr_sum, stack, i + 1)
+                curr_sum -= candidates[i]
+                stack.pop()
         
-        for i in range(cur, len(cand)):
-            if i > cur and cand[i] == cand[i - 1]:  # Skip duplicates
-                continue
-            path.append(cand[i])
-            self.dfs(cand, i + 1, target - cand[i], path, res)
-            path.pop()  # Backtrack
+        dfs(0, [], 0)
+        return res
+        
