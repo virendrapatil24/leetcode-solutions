045d4da00873e42e52a0d64c66a94b3f4a05d780
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2025-03-26 16:08:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.1 MB (21.31%)

--- leetcode/0078-subsets/solution.py
+++ leetcode/0078-subsets/solution.py
@@ -1,20 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        res = []
-
-        def dfs(stack, idx):
-            if idx == n:
-                res.append(stack[:])
-                return 
-
-            stack.append(nums[idx])
-            dfs(stack, idx + 1)
-
-            stack.pop()
-            dfs(stack, idx + 1)
-
-        dfs([], 0)
-        return res
+        subsets = []
+        len_subsets = 1 << len(nums)
+        for i in range(len_subsets):
+            curr_subset = []
+            for j in range(len(nums)):
+                if i & (1 << j) > 0:
+                    curr_subset.append(nums[j])
+            subsets.append(curr_subset)
+        return subsets
 
         
