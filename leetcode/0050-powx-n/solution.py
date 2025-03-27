907c55f8fa91f1012b22ae688bfee6e1ddc6c13d
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2025-03-27 17:49:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (74.60%)

--- leetcode/0050-powx-n/solution.py
+++ leetcode/0050-powx-n/solution.py
@@ -2,7 +2,7 @@ class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n == 0:
             return float(1)
-        
+    
         if n < 0:
             n = -n
             x = 1 / x
@@ -11,4 +11,5 @@ class Solution:
             return self.myPow(x * x, n // 2)
         else:
             return x * self.myPow(x * x, n // 2)
+
         
