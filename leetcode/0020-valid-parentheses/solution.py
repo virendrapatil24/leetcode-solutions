ea4dc16cba5ddc6d292105734cc9bce0d05da06e
Virendra Patil <70162563+virendrapatil24@users.noreply.github.com>
2025-03-29 06:33:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (32.19%)

--- leetcode/0020-valid-parentheses/solution.py
+++ leetcode/0020-valid-parentheses/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) % 2 != 0: return False
-        isMatch = {"(":")", "{":"}", "[":"]"}
+        if len(s) & 1: return False
+        para_map = {'(':')','{':'}','[':']'}
         stack = []
         for char in s:
-            if char in isMatch:
+            if char in para_map:
                 stack.append(char)
-            elif not stack or isMatch[stack.pop()] != char:
+            elif not stack or char != para_map[stack.pop()]:
                 return False
         return len(stack) == 0
+
 
         
